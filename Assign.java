18d038c385f6374a2b0c2e1831bfb6b8c0f81428
sxndeepthakurr <sandeepthakurtaroli@gamil.com>
2025-08-13 10:48:46 +0530

    [Sandeep Sisodiya] Assign.java

---
+++ Assign.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Assign{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        // //Question 1
+        // System.out.println("Welcome TO BridgeLabz!");
+
+        // //Question 2 Add two number
+        // int num1=sc.nextInt();
+        // int num2=sc.nextInt();
+        // System.out.println("Sum of two number = "+(num1+num2));
+
+
+        // //Question 3 Celsius to Fahrenheit Conversion
+        // System.out.println("Enter Temperature in celcius");
+        // int temp=sc.nextInt();
+        // int fehrenheit=(temp*(9/5))+32;
+        // System.out.println("Temperature in Fehrenheit="+fehrenheit);
+
+        //  question 4 Area of Circle
+        // int radius=sc.nextInt();
+        // double area=Math.PI*radius*radius;
+        // System.out.println("Area of Circle is ="+area);
+
+
+        //Q5 Volume of a cylinder
+        int radius=sc.nextInt();
+        int height=sc.nextInt();
+        double vol=Math.PI*radius*radius*height;
+
+        System.out.println("Volume of a Cylinder ="+ vol);
+
+        //Self Problems
+        //Calculate simple Interest
+
+    }
+}
